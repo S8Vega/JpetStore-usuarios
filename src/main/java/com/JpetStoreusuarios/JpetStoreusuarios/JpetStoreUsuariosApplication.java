430ae310655338baf66c12779dcd6cefd3a38772
bsvega <bsvega@bancolombia.com.co>
2021-11-16 20:51:17 -0500

    configuracion inicial del proyecto

---
+++ src/main/java/com/JpetStoreusuarios/JpetStoreusuarios/JpetStoreUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.JpetStoreusuarios.JpetStoreusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetStoreUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpetStoreUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JpetStoreusuarios/JpetStoreusuarios/JpetStoreUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JpetStoreusuarios.JpetStoreusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpetStoreUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
