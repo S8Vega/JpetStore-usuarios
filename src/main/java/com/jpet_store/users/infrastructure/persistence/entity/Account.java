289163b89353122ad3d2e90a7fb839a61ee7c4b8
bsvega <bsvega@bancolombia.com.co>
2021-11-16 21:14:35 -0500

    add entitys of the db

---
+++ src/main/java/com/jpet_store/users/infrastructure/persistence/entity/Account.java
@@ -0,0 +1,28 @@
+package com.jpet_store.users.infrastructure.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Account extends User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private String fullName;
+}
---
+++ src/main/java/com/jpet_store/users/infrastructure/persistence/entity/User.java
@@ -0,0 +1,25 @@
+package com.jpet_store.users.infrastructure.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String password;
+
+}
